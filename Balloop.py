6012473ee50c847f158c9cec482e363206536421
BocchiProgram <127451217+BocchiProgram@users.noreply.github.com>
2023-07-16 18:50:04 -0300

    Alterações pra fluidez do jogo

--- Balloop.py
+++ Balloop.py
@@ -11,45 +11,50 @@ class Jogo:
         clock = pygame.time.Clock()
 
         pygame.init()
-        self.contador = 1
+        pygame.display.set_caption('Regular Game')
+
         largura = 1080
         altura = 650
         dt = 0
 
+        #Contador de levels e pontos
+        self.pontos = 0 #Conta pontos
+        self.contador = 1 #Conta levels
+
         self.tela = pygame.display.set_mode((largura, altura))
-        pygame.display.set_caption('Regular Game')
+        self.fundo_cor = Transicions.Trans.backgroud_color(self.contador)
+        self.fonte = pygame.font.SysFont('arial', 40, True, True)
+        
 
         self.player_pos = pygame.Vector2(self.tela.get_width() / 2, self.tela.get_height() / 2)
         self.player_massa = 20
-        self.comida = []
-        self.lugares_aletorios = []
-
-        self.player_cor = 'black'
-        self.fundo_cor = 'purple'
-
+        self.player_cor = Transicions.Trans.player_color(self.contador)
         self.player_velocidade = 235
 
+        
+        self.comida = []
+        self.pontos_por_comida = 5
         self.massa_comida = 5
-
+        self.lugares_aletorios = []
+        
         self.criar_massas(20)
+   
 
         while True: 
+
+            self.mensagem = f'Pontos: {self.pontos}'
+            self.texto_formatado = self.fonte.render(self.mensagem, True, (255, 255, 255))
+
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
                     exit()
 
             self.tela.fill(self.fundo_cor)
-            try:
-                self.manter_massas(20)
-            except:
-                pass
+            self.manter_massas(20)
             
             #player 
-            try:
-                self.player = pygame.draw.circle(self.tela, self.player_cor, self.player_pos, self.player_massa)   
-            except:
-                pass
+            self.player = pygame.draw.circle(self.tela, self.player_cor, self.player_pos, self.player_massa)   
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_w]:
@@ -61,32 +66,26 @@ class Jogo:
             if keys[pygame.K_s]:
                 self.player_pos.y += self.player_velocidade * dt
 
-            self.pontos_por_comida = 5
-
             self.colisao()
             
-            if self.player_massa > 400:
-                self.pontos_por_comida = 1
-            try:
-                if self.player_massa > 1500:
-                    self.player_velocidade = 235
-                    self.massa_comida = 5
-                    self.player_massa = 20
-                    self.contador += 1
-                    self.fundo_cor = Transicions.Trans.backgroud_color(self.contador)
-                    while True:
-                        self.player_cor = Transicions.Trans.player_color(self.contador) 
-                        if self.player_cor != self.fundo_cor:
-                            break
-            except:
-                pass
+            if self.player_massa > 1500:
+                self.player_velocidade = 235
+                self.massa_comida = 5
+                self.player_massa = 20
+                self.contador += 1
+                self.pontos_por_comida = 5
+                self.fundo_cor = Transicions.Trans.backgroud_color(self.contador)
+                while True:
+                    self.player_cor = Transicions.Trans.player_color(self.contador) 
+                    if self.player_cor != self.fundo_cor:
+                        break
 
             if self.player_massa > 100 and self.player_massa < 200:
                 self.player_velocidade = 200
-                self.pontos_por_comida = 0.1
-            if self.player_massa > 200:
+                self.pontos_por_comida = 4
+            if self.player_massa > 300:
                 self.player_velocidade = 250
-                self.pontos_por_comida = 0.00000000000001
+                self.pontos_por_comida = 3
 
             pygame.display.flip()
 
@@ -117,15 +116,17 @@ class Jogo:
             return True
         else:
             return False
-    
+
     def colisao(self):
         for c, comida in enumerate(self.comida):
             if self.verificar_colisao((self.player_pos.x, self.player_pos.y, self.player_massa), (comida[0], comida[1], comida[2])):
                 del self.comida[c]
                 del self.lugares_aletorios[c]
                 self.player_massa += self.pontos_por_comida
+                self.pontos += 1
                 self.criar_massas(1)
                     
+        self.tela.blit(self.texto_formatado, (10, 10))
 
 if __name__ == "__main__":
     Jogo()
--- Transicions.py
+++ Transicions.py
@@ -1,6 +1,6 @@
-import Balloop
 import random
-cores = ['purple', 'black'], ['black', 'red',], ['red', 'green'], ['green', 'black'], ['black', 'purple'], ['purple', 'black']
+cores = ['black', 'red'], ['red', 'black'], ['black', 'green'], ['green', 'black'], ['black', 'pink'], ['pink', 'black'],['black', 'yellow'], ['yellow', 'black']
+
 class Trans:
     # def init(self, x):
     #     for c in range()
@@ -8,9 +8,9 @@ class Trans:
     def backgroud_color(x):
         if x > len(cores): # caso acabe as programadas, começa a gerar cores aleatórias!
             x = random.randrange(len(cores))    
-        return cores[x - 1][0]
+        return cores[x][0]
 
     def player_color(x):
         if x > len(cores): # caso acabe as programadas, começa a gerar cores aleatórias!
             x = random.randrange(len(cores))    
-        return cores[x - 1][1]
+        return cores[x][1]
